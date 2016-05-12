83177fc4fca8f1494c99a426aa7c3629207d98a6
Raragon1911 <havok3030@gmail.com>
2016-05-12 15:10:43 -0600

    first change

--- GradebookApp.java
+++ GradebookApp.java
@@ -3,7 +3,17 @@
 class GradebookApp {
 	
 	public static void main(String[] args) {
+		Student s = new Student("Ramos");
+		s.getGrades();
 
+		double highest = s.A_grades();
+		System.out.print(highest);
+
+		double lowest = s.F_grades();
+		System.out.print(lowest);
+
+		double average = s.avg_grade();
+		System.out.print(average);
 	}
 
 }
--- Student.java
+++ Student.java
@@ -1,5 +1,45 @@
 // Student.java v.20160502
 
 class Student {
+	double [] grades = new double[10];
+	String name;
+
+		public Student(String n){
+			name = n;
+
+			for(int i = 0; i < grades.length;i++){
+				grades[i]=(Math.random()*100 + 1);
+			}
+		}
+		public void getGrades(){
+			System.out.print(name+"\n\n");
+			for(int i = 0; i <grades.length;i++){
+				System.out.println(grades[i]);
+			}
+		}
+		public double A_grades(){
+			double max = grades[0];
+			for(int i = 0; i <grades.length;i++){
+				if(grades[i]>max){
+					max = grades[i];
+				}
+			}return max;
+		}
+		public double F_grades(){
+			double min = grades[0];
+			for(int i = 0;i <grades.length;i++){
+				if(grades[i]<min){
+					min = grades[i];
+				}
+			}return min;
+		}
+		public double avg_grade(){
+			double sum = 0;
+			for(int i = 0;i <grades.length;i++){
+				sum = grades[i]+sum;
+			}
+			double avg = sum / grades.length;
+			return avg;
+		}
 
 }
